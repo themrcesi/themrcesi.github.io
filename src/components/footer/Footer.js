dfafcac534c87a2009008c031490fed82741479d
César García Cabeza <UO257314@uniovi.es>
2020-06-02 23:48:03 +0200

    NavBar & Intro done

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="container">
+                    <div className="row">
+                    <div className="col-sm-12">
+                        <div className="copyright-box">
+                        <p className="copyright">2020 &copy; Copyright <strong>César García Cabeza</strong>. All Rights Reserved</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {MyNavBar} from './components/navBar/MyNavBar';
+import {Intro} from './components/intro/Intro';
+import About from './components/about/About';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
+import BackToTop from './components/backToTop/BackToTop';
+import Preloader from './components/preloader/Preloader';
+
+import "./style.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.Fragment>
+    <MyNavBar></MyNavBar>
+    <Intro></Intro>
+    <About></About>
+    {/* <Projects></Projects>
+    <Contact></Contact>
+    <BackToTop></BackToTop>
+    <Preloader></Preloader> */}
+  </React.Fragment>,
   document.getElementById('root')
 );
 
