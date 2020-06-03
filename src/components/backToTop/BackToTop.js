6ca98952bf448a00139fa4c11925c48982ca4fe7
César García Cabeza <UO257314@uniovi.es>
2020-06-03 21:05:50 +0200

    Working

---
+++ src/components/backToTop/BackToTop.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BackToTop extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import {MyNavBar} from './components/navBar/MyNavBar';
+import {Intro} from './components/intro/Intro';
+import About from './components/about/About';
+import Projects from './components/projects/Projects';
+/* import Contact from './components/contact/Contact';
+import BackToTop from './components/backToTop/BackToTop';
+import Preloader from './components/preloader/Preloader'; */
+
+import "./style.css";
+
+ReactDOM.render(
+  <React.Fragment>
+    <MyNavBar></MyNavBar>
+    <Intro></Intro>
+    <About></About>
+    <Projects></Projects>
+    {/* <Contact></Contact>
+    <BackToTop></BackToTop>
+    <Preloader></Preloader> */}
+  </React.Fragment>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
