536d45944fabfdbf795794088e0823e8a95f819a
César García Cabeza <UO257314@uniovi.es>
2020-06-03 19:27:54 +0200

    Updates

---
+++ precache-manifest.221939244639f39d3d1d52ee517e5776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0144b7807a02c7ebffa4090e5ac487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e707a5a286d8feafc94",
+    "url": "/static/css/main.75f49eeb.chunk.css"
+  },
+  {
+    "revision": "dd4b339e2e34a7e1b8e0",
+    "url": "/static/js/2.9a7c6f90.chunk.js"
+  },
+  {
+    "revision": "89f783f3178d590ca7b4568eae1f51ea",
+    "url": "/static/js/2.9a7c6f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e707a5a286d8feafc94",
+    "url": "/static/js/main.ba8806b1.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
