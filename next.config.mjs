9a57e5244fcfd2c4125d9b1e452c9b2828579b74
cgarcia <cgarcia@g-n.com>
2024-08-23 01:09:38 +0200

    New web

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    assetPrefix: !debug ? 'https://themrcesi.github.io/' : ''
 };
 
 export default nextConfig;
